b9108d2d5254aaaa5bba90634adb29ce1646bc4b
Dewald Els <dewald@sumodevelopment.co.za>
2017-05-19 13:28:17 +0200

    Operators challenge
    Operators

---
+++ 30-day-challenge/operators.js
@@ -0,0 +1,13 @@
+function main() {
+    
+    let calcFromPerc = function(val) {
+        return val / 100;
+    };
+    
+    let total = 0;
+    let tip = mealCost * calcFromPerc(tipPercent);
+    let tax = mealCost * calcFromPerc(taxPercent);
+    total = parseInt(mealCost + tip + tax);
+    let message = `The total meal cost is ${total} dollars.`;
+    console.log(message);
+}
