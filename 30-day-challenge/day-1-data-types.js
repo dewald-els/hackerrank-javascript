0ccec82484a8c9f4174e812d11653c8d3c2a85aa
Dewald Els <dewald@sumodevelopment.co.za>
2017-05-19 13:47:44 +0200

    Data types
    I can’t figure out a better way to work with decimals. Any help would
    be greatly appreciated.

---
+++ 30-day-challenge/day-1-data-types.js
@@ -0,0 +1,21 @@
+function main() {
+    // Declare second integer, double, and String variables.
+    let myI = 0;
+    let myD = 0.0;
+    let myS = "";
+    // Read and save an integer, double, and String to your variables.
+    // There has to be a better way to do this in Javascript?!
+    d   = parseFloat(d).toPrecision(2);
+    myI = parseInt(readLine());
+    myD = parseFloat(readLine()).toPrecision(2);
+    myS = readLine();
+    // Print the sum of both integer variables on a new line.
+    console.log(myI + i);
+    
+    // Print the sum of the double variables on a new line.
+    // There has to be a better way to do this in Javascript?!
+    console.log((parseFloat(d) + parseFloat(myD)).toPrecision(2));
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    console.log(s + myS);
+}
